d813837089b455de6de164683fcfa22ef33cd3d8
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-01 14:58:25 -0500

    test commit

---
+++ mysite/newapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Movies(models.Model):
+    
+    name = models.CharField(max_length=200)
+    rating = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
